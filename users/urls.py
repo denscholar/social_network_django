b74cd9fdcb414b9cd408bd9053d3102bd0d891f7
Dennis Akagha <dennis.akagha@afripointgroup.com>
2023-03-31 07:43:20 +0100

    Add the registration features for the app

--- users/urls.py
+++ users/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
         template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path("password_reset_complete/", auth_views.PasswordResetCompleteView.as_view(
         template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+    path('register/', views.register, name='register'),
 ]
